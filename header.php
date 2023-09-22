7196d42d126ec39803028301103b157ef274d45a
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2023-09-22 18:12:41 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<!--
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	-->
+	<link rel="stylesheet" href="/wp-content/themes/Revelio/style.css">
+	
+	<title><?php bloginfo('title') ?></title>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>
+	  
+	  <div id="container">
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+
+<h1> Reducto! </h1>
+
+<?php get_footer() ?>
