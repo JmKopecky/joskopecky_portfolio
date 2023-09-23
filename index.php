c97d1bb9291b7788e5a66a205672169aa95eb22d
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2023-09-23 09:18:50 -0500

    Created typography and color variables.

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php get_header() ?>
 
-//put your content here!
+<!-- Put your content here! -->
+
+<div class="test-area"> 
+    <h1> Revelio! </h1>
+    <h2> Revelio! </h2>
+    <h3> Revelio! </h3>
+    <h4> Revelio! </h4>
+    <h5> Revelio! </h5>
+    <h6> Revelio! </h6>
+    <p> Casting <a href="http://revelio.local"> Revelio! </a> will show hidden things. </p>
+    <button> <a href="http://revelio.local"> Revelio! </a> </button>
+    <button> <p> Revelio! </p> </button>
+</div>
 
 <?php get_footer() ?>
