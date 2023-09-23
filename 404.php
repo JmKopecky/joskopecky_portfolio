7f4df430956c8670a40540b18e91807a0058fd28
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2023-09-23 07:33:05 -0500

    Added 404 template.

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+
+<div class="page-404"> 
+    <h1 class="title-404"> 404 </h1>
+    <p class="message-404"> Someone messed up and I don't know for sure who it is but you might want to check your link. </p>
+</div>
+
+<?php get_footer() ?>
